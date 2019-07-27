dddf382e88877ce4889909fb5b655d3b96be3930
Damien Gibson <SpiceyWolf.ArchaicSoft@outlook.com>
2019-07-27 18:31:24 -0400

    Fixed the npm-binding Copy
    So it doesnt pull the useless stuff from mupen64plus.node compile

--- build_mupen_win32.bat
+++ build_mupen_win32.bat
@@ -4,4 +4,4 @@ copy Mupen64Plus_Bundle_Linux_x64\* build\emulator
 cd ModLoader64-M64P-Npm
 call script_build.bat
 cd ..
-copy ModLoader64-M64P-Npm\build\Release build\emulator
+copy ModLoader64-M64P-Npm\build\Release\mupen64plus.node build\emulator\
