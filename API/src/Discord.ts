4e3c2fa48e1d1b7144d8b087fd1adccc70c016c8
Denoflions <denvstheworld@gmail.com>
2019-10-31 15:27:00 -0400

    add more fields to discord.

--- API/src/Discord.ts
+++ API/src/Discord.ts
@@ -2,12 +2,18 @@ export interface IDiscordStatus {
   details: string;
   state: string;
   smallImageKey?: string;
+  partyId?: string;
+  partySize?: number;
+  partyMax?: number;
 }
 
 export class DiscordStatus implements IDiscordStatus {
   details: string;
   state: string;
   smallImageKey?: string | undefined;
+  partyId?: string | undefined;
+  partySize?: number | undefined;
+  partyMax?: number | undefined;
 
   constructor(details: string, state: string) {
     this.details = details;
