a1a01a5ee6abda052f01cc943946e985316cd4eb
SpiceyWolf <spiceywolf.archaicsoft@outlook.com>
2019-11-06 11:48:52 -0500

    Less problematic Inventory API (OOT CORE)

--- src/modloader/cores/OOT/Inventory.ts
+++ src/modloader/cores/OOT/Inventory.ts
@@ -400,362 +400,335 @@ export class Inventory extends JSONTemplate implements IInventory {
   }
 
   get dekuSticks(): boolean {
-    return this.hasItem(InventoryItem.DEKU_STICK);
+    let val = this.getItemInSlot(InventorySlots.DEKU_STICKS);
+    return !(val === InventoryItem.NONE);
   }
   set dekuSticks(bool: boolean) {
-    if (bool) {
-      this.giveItem(InventoryItem.DEKU_STICK, InventorySlots.DEKU_STICKS);
-    } else {
-      this.removeItem(InventoryItem.DEKU_STICK);
-    }
+    let value = bool ? InventoryItem.DEKU_STICK : InventoryItem.NONE;
+    this.setItemInSlot(value, InventorySlots.DEKU_STICKS)
   }
+
   get dekuSticksCount(): number {
-    return this.getAmmoForItem(InventoryItem.DEKU_STICK);
+    return this.getAmmoForSlot(InventorySlots.DEKU_STICKS);
   }
   set dekuSticksCount(count: number) {
-    let slot = this.getSlotForItem(InventoryItem.DEKU_STICK);
-    this.setAmmoInSlot(slot, count);
+    this.setAmmoInSlot(InventorySlots.DEKU_STICKS, count);
   }
+
   get dekuNuts(): boolean {
-    return this.hasItem(InventoryItem.DEKU_NUT);
+    let val = this.getItemInSlot(InventorySlots.DEKU_NUTS);
+    return !(val === InventoryItem.NONE);
   }
   set dekuNuts(bool: boolean) {
-    if (bool) {
-      this.giveItem(InventoryItem.DEKU_NUT, InventorySlots.DEKU_NUTS);
-    } else {
-      this.removeItem(InventoryItem.DEKU_NUT);
-    }
+    let value = bool ? InventoryItem.DEKU_NUT : InventoryItem.NONE;
+    this.setItemInSlot(value, InventorySlots.DEKU_NUTS);
   }
+
   get dekuNutsCount(): number {
-    return this.getAmmoForItem(InventoryItem.DEKU_NUT);
+    return this.getAmmoForSlot(InventorySlots.DEKU_NUTS);
   }
   set dekuNutsCount(count: number) {
-    let slot = this.getSlotForItem(InventoryItem.DEKU_NUT);
-    this.setAmmoInSlot(slot, count);
+    this.setAmmoInSlot(InventorySlots.DEKU_NUTS, count);
   }
+
   get bombs(): boolean {
-    return this.hasItem(InventoryItem.BOMB);
+    let val = this.getItemInSlot(InventorySlots.BOMBS);
+    return !(val === InventoryItem.NONE);
   }
   set bombs(bool: boolean) {
-    if (bool) {
-      this.giveItem(InventoryItem.BOMB, InventorySlots.BOMBS);
-    } else {
-      this.removeItem(InventoryItem.BOMB);
-    }
+    let value = bool ? InventoryItem.BOMB : InventoryItem.NONE;
+    this.setItemInSlot(value, InventorySlots.BOMBS);
   }
+
   get bombsCount(): number {
-    return this.getAmmoForItem(InventoryItem.BOMB);
+    return this.getAmmoForSlot(InventorySlots.BOMBS);
   }
   set bombsCount(count: number) {
-    let slot = this.getSlotForItem(InventoryItem.BOMB);
-    this.setAmmoInSlot(slot, count);
+    this.setAmmoInSlot(InventorySlots.BOMBS, count);
   }
+
   get bombchus(): boolean {
-    return this.hasItem(InventoryItem.BOMBCHU);
+    let val = this.getItemInSlot(InventorySlots.BOMBCHUS);
+    return !(val === InventoryItem.NONE);
   }
   set bombchus(bool: boolean) {
-    if (bool) {
-      this.giveItem(InventoryItem.BOMBCHU, InventorySlots.BOMBCHUS);
-    } else {
-      this.removeItem(InventoryItem.BOMBCHU);
-    }
+    let value = bool ? InventoryItem.BOMBCHU : InventoryItem.NONE;
+    this.setItemInSlot(value, InventorySlots.BOMBCHUS);
   }
+
   get bombchuCount(): number {
-    return this.getAmmoForItem(InventoryItem.BOMBCHU);
+    return this.getAmmoForSlot(InventorySlots.BOMBCHUS);
   }
   set bombchuCount(count: number) {
-    let slot = this.getSlotForItem(InventoryItem.BOMBCHU);
-    this.setAmmoInSlot(slot, count);
+    this.setAmmoInSlot(InventorySlots.BOMBCHUS, count);
   }
+
   get magicBeans(): boolean {
-    return this.hasItem(InventoryItem.MAGIC_BEAN);
+    let val = this.getItemInSlot(InventorySlots.MAGIC_BEANS);
+    return !(val === InventoryItem.NONE);
   }
   set magicBeans(bool: boolean) {
-    if (bool) {
-      this.giveItem(InventoryItem.MAGIC_BEAN, InventorySlots.MAGIC_BEANS);
-    } else {
-      this.removeItem(InventoryItem.MAGIC_BEAN);
-    }
+    let value = bool ? InventoryItem.MAGIC_BEAN : InventoryItem.NONE;
+    this.setItemInSlot(value, InventorySlots.MAGIC_BEANS);
   }
+
   get magicBeansCount(): number {
-    return this.getAmmoForItem(InventoryItem.MAGIC_BEAN);
+    return this.getAmmoForSlot(InventorySlots.MAGIC_BEANS);
   }
   set magicBeansCount(count: number) {
-    let slot = this.getSlotForItem(InventoryItem.MAGIC_BEAN);
-    this.setAmmoInSlot(slot, count);
+    this.setAmmoInSlot(InventorySlots.MAGIC_BEANS, count);
   }
+
   get fairySlingshot(): boolean {
-    return this.hasItem(InventoryItem.FAIRY_SLINGSHOT);
+    let val = this.getItemInSlot(InventorySlots.FAIRY_SLINGSHOT);
+    return !(val === InventoryItem.NONE);
   }
   set fairySlingshot(bool: boolean) {
-    if (bool) {
-      this.giveItem(
-        InventoryItem.FAIRY_SLINGSHOT,
-        InventorySlots.FAIRY_SLINGSHOT
-      );
-    } else {
-      this.removeItem(InventoryItem.FAIRY_SLINGSHOT);
-    }
+    let value = bool ? InventoryItem.FAIRY_SLINGSHOT : InventoryItem.NONE;
+    this.setItemInSlot(value, InventorySlots.FAIRY_SLINGSHOT);
   }
+
   get dekuSeeds(): number {
-    return this.getAmmoForItem(InventoryItem.FAIRY_SLINGSHOT);
+    return this.getAmmoForSlot(InventorySlots.FAIRY_SLINGSHOT);
   }
   set dekuSeeds(count: number) {
-    let slot = this.getSlotForItem(InventoryItem.FAIRY_SLINGSHOT);
-    this.setAmmoInSlot(slot, count);
+    this.setAmmoInSlot(InventorySlots.FAIRY_SLINGSHOT, count);
   }
+
   get fairyBow(): boolean {
-    return this.hasItem(InventoryItem.FAIRY_BOW);
+    let val = this.getItemInSlot(InventorySlots.FAIRY_BOW);
+    return !(val === InventoryItem.NONE);
   }
   set fairyBow(bool: boolean) {
-    if (bool) {
-      this.giveItem(InventoryItem.FAIRY_BOW, InventorySlots.FAIRY_BOW);
-    } else {
-      this.removeItem(InventoryItem.FAIRY_BOW);
-    }
+    let value = bool ? InventoryItem.FAIRY_BOW : InventoryItem.NONE;
+    this.setItemInSlot(value, InventorySlots.FAIRY_BOW);
   }
+
   get arrows(): number {
-    return this.getAmmoForItem(InventoryItem.FAIRY_BOW);
+    return this.getAmmoForSlot(InventorySlots.FAIRY_BOW);
   }
   set arrows(count: number) {
-    let slot = this.getSlotForItem(InventoryItem.FAIRY_BOW);
-    this.setAmmoInSlot(slot, count);
+    this.setAmmoInSlot(InventorySlots.FAIRY_BOW, count);
   }
+
   get fireArrows(): boolean {
-    return this.hasItem(InventoryItem.FIRE_ARROW);
+    let val = this.getItemInSlot(InventorySlots.FIRE_ARROWS);
+    return !(val === InventoryItem.NONE);
   }
   set fireArrows(bool: boolean) {
-    if (bool) {
-      this.giveItem(InventoryItem.FIRE_ARROW, InventorySlots.FIRE_ARROWS);
-    } else {
-      this.removeItem(InventoryItem.FIRE_ARROW);
-    }
+    let value = bool ? InventoryItem.FIRE_ARROW : InventoryItem.NONE;
+    this.setItemInSlot(value, InventorySlots.FIRE_ARROWS);
   }
+
   get iceArrows(): boolean {
-    return this.hasItem(InventoryItem.ICE_ARROW);
+    let val = this.getItemInSlot(InventorySlots.ICE_ARROWS);
+    return !(val === InventoryItem.NONE);
   }
   set iceArrows(bool: boolean) {
-    if (bool) {
-      this.giveItem(InventoryItem.ICE_ARROW, InventorySlots.ICE_ARROWS);
-    } else {
-      this.removeItem(InventoryItem.ICE_ARROW);
-    }
+    let value = bool ? InventoryItem.ICE_ARROW : InventoryItem.NONE;
+    this.setItemInSlot(value, InventorySlots.ICE_ARROWS);
   }
+
   get lightArrows(): boolean {
-    return this.hasItem(InventoryItem.LIGHT_ARROW);
+    let val = this.getItemInSlot(InventorySlots.LIGHT_ARROWS);
+    return !(val === InventoryItem.NONE);
   }
   set lightArrows(bool: boolean) {
-    if (bool) {
-      this.giveItem(InventoryItem.LIGHT_ARROW, InventorySlots.LIGHT_ARROWS);
-    } else {
-      this.removeItem(InventoryItem.LIGHT_ARROW);
-    }
+    let value = bool ? InventoryItem.LIGHT_ARROW : InventoryItem.NONE;
+    this.setItemInSlot(value, InventorySlots.LIGHT_ARROWS);
   }
+
   get dinsFire(): boolean {
-    return this.hasItem(InventoryItem.DINS_FIRE);
+    let val = this.getItemInSlot(InventorySlots.DINS_FIRE);
+    return !(val === InventoryItem.NONE);
   }
   set dinsFire(bool: boolean) {
-    if (bool) {
-      this.giveItem(InventoryItem.DINS_FIRE, InventorySlots.DINS_FIRE);
-    } else {
-      this.removeItem(InventoryItem.DINS_FIRE);
-    }
+    let value = bool ? InventoryItem.DINS_FIRE : InventoryItem.NONE;
+    this.setItemInSlot(value, InventorySlots.DINS_FIRE);
   }
+
   get faroresWind(): boolean {
-    return this.hasItem(InventoryItem.FARORES_WIND);
+    let val = this.getItemInSlot(InventorySlots.FARORES_WIND);
+    return !(val === InventoryItem.NONE);
   }
   set faroresWind(bool: boolean) {
-    if (bool) {
-      this.giveItem(InventoryItem.FARORES_WIND, InventorySlots.FARORES_WIND);
-    } else {
-      this.removeItem(InventoryItem.FARORES_WIND);
-    }
+    let value = bool ? InventoryItem.FARORES_WIND : InventoryItem.NONE;
+    this.setItemInSlot(value, InventorySlots.FARORES_WIND);
   }
+
   get nayrusLove(): boolean {
-    return this.hasItem(InventoryItem.NAYRUS_LOVE);
+    let val = this.getItemInSlot(InventorySlots.NAYRUS_LOVE);
+    return !(val === InventoryItem.NONE);
   }
   set nayrusLove(bool: boolean) {
-    if (bool) {
-      this.giveItem(InventoryItem.NAYRUS_LOVE, InventorySlots.NAYRUS_LOVE);
-    } else {
-      this.removeItem(InventoryItem.NAYRUS_LOVE);
-    }
+    let value = bool ? InventoryItem.NAYRUS_LOVE : InventoryItem.NONE;
+    this.setItemInSlot(value, InventorySlots.NAYRUS_LOVE);
   }
   get ocarina(): Ocarina {
-    if (this.hasItem(InventoryItem.OCARINA_OF_TIME)) {
-      return Ocarina.OCARINA_OF_TIME;
+    let val = this.getItemInSlot(InventorySlots.OCARINA);
+    switch (val) {
+      case InventoryItem.FAIRY_OCARINA:
+        return Ocarina.FAIRY_OCARINA;
+      case InventoryItem.OCARINA_OF_TIME:
+        return Ocarina.OCARINA_OF_TIME;
+      default:
+        return Ocarina.NONE;
     }
-    if (this.hasItem(InventoryItem.FAIRY_OCARINA)) {
-      return Ocarina.FAIRY_OCARINA;
-    }
-    return Ocarina.NONE;
   }
   set ocarina(item: Ocarina) {
-    if (item == this.ocarina) {
-      return;
-    }
-    if (item == Ocarina.NONE) {
-      this.removeItem(InventoryItem.OCARINA_OF_TIME);
-      this.removeItem(InventoryItem.FAIRY_OCARINA);
-    }
-    if (item == Ocarina.OCARINA_OF_TIME) {
-      let slot: number = this.getSlotForItem(InventoryItem.FAIRY_OCARINA);
-      if (slot > -1) {
-        this.setItemInSlot(InventoryItem.OCARINA_OF_TIME, slot);
-      } else {
-        this.giveItem(InventoryItem.OCARINA_OF_TIME, InventorySlots.OCARINA);
-      }
-    }
-    if (item == Ocarina.FAIRY_OCARINA) {
-      let slot: number = this.getSlotForItem(InventoryItem.OCARINA_OF_TIME);
-      if (slot > -1) {
-        this.setItemInSlot(InventoryItem.FAIRY_OCARINA, slot);
-      } else {
-        this.giveItem(InventoryItem.FAIRY_OCARINA, InventorySlots.OCARINA);
-      }
+    if (item === this.ocarina) return;
+
+    switch (item) {
+      case Ocarina.NONE:
+        this.setItemInSlot(InventoryItem.NONE, InventorySlots.OCARINA);
+        break;
+      case Ocarina.FAIRY_OCARINA:
+        this.setItemInSlot(InventoryItem.FAIRY_OCARINA, InventorySlots.OCARINA);
+        break;
+      case Ocarina.OCARINA_OF_TIME:
+        this.setItemInSlot(InventoryItem.OCARINA_OF_TIME, InventorySlots.OCARINA);
+        break;
     }
   }
+
   get hookshot(): Hookshot {
-    if (this.hasItem(InventoryItem.LONGSHOT)) {
-      return Hookshot.LONGSHOT;
+    let val = this.getItemInSlot(InventorySlots.HOOKSHOT);
+    switch (val) {
+      case InventoryItem.HOOKSHOT:
+        return Hookshot.HOOKSHOT;
+      case InventoryItem.LONGSHOT:
+        return Hookshot.LONGSHOT;
+      default:
+        return Hookshot.NONE;
     }
-    if (this.hasItem(InventoryItem.HOOKSHOT)) {
-      return Hookshot.HOOKSHOT;
-    }
-    return Hookshot.NONE;
   }
   set hookshot(item: Hookshot) {
-    if (item == this.hookshot) {
-      return;
-    }
-    if (item == Hookshot.NONE) {
-      this.removeItem(InventoryItem.HOOKSHOT);
-      this.removeItem(InventoryItem.LONGSHOT);
-    }
-    if (item == Hookshot.LONGSHOT) {
-      let slot: number = this.getSlotForItem(InventoryItem.HOOKSHOT);
-      if (slot > -1) {
-        this.setItemInSlot(InventoryItem.LONGSHOT, slot);
-      } else {
-        this.giveItem(InventoryItem.LONGSHOT, InventorySlots.HOOKSHOT);
-      }
-    }
-    if (item == Hookshot.HOOKSHOT) {
-      let slot: number = this.getSlotForItem(InventoryItem.LONGSHOT);
-      if (slot > -1) {
-        this.setItemInSlot(InventoryItem.HOOKSHOT, slot);
-      } else {
-        this.giveItem(InventoryItem.HOOKSHOT, InventorySlots.HOOKSHOT);
-      }
+    if (item === this.hookshot) return;
+
+    switch (item) {
+      case Hookshot.NONE:
+        this.setItemInSlot(InventoryItem.NONE, InventorySlots.HOOKSHOT);
+        break;
+      case Hookshot.HOOKSHOT:
+        this.setItemInSlot(InventoryItem.HOOKSHOT, InventorySlots.HOOKSHOT);
+        break;
+      case Hookshot.LONGSHOT:
+        this.setItemInSlot(InventoryItem.LONGSHOT, InventorySlots.HOOKSHOT);
+        break;
     }
   }
+
   get boomerang(): boolean {
-    return this.hasItem(InventoryItem.BOOMERANG);
+    let val = this.getItemInSlot(InventorySlots.BOOMERANG);
+    return !(val === InventoryItem.NONE);
   }
   set boomerang(bool: boolean) {
-    if (bool) {
-      this.giveItem(InventoryItem.BOOMERANG, InventorySlots.BOOMERANG);
-    } else {
-      this.removeItem(InventoryItem.BOOMERANG);
-    }
+    let value = bool ? InventoryItem.BOOMERANG : InventoryItem.NONE;
+    this.setItemInSlot(value, InventorySlots.BOOMERANG);
   }
+
   get lensOfTruth(): boolean {
-    return this.hasItem(InventoryItem.LENS_OF_TRUTH);
+    let val = this.getItemInSlot(InventorySlots.LENS_OF_TRUTH);
+    return !(val === InventoryItem.NONE);
   }
   set lensOfTruth(bool: boolean) {
-    if (bool) {
-      this.giveItem(InventoryItem.LENS_OF_TRUTH, InventorySlots.LENS_OF_TRUTH);
-    } else {
-      this.removeItem(InventoryItem.LENS_OF_TRUTH);
-    }
+    let value = bool ? InventoryItem.LENS_OF_TRUTH : InventoryItem.NONE;
+    this.setItemInSlot(value, InventorySlots.LENS_OF_TRUTH);
   }
+
   get megatonHammer(): boolean {
-    return this.hasItem(InventoryItem.MEGATON_HAMMER);
+    let val = this.getItemInSlot(InventorySlots.MEGATON_HAMMER);
+    return !(val === InventoryItem.NONE);
   }
   set megatonHammer(bool: boolean) {
-    if (bool) {
-      this.giveItem(
-        InventoryItem.MEGATON_HAMMER,
-        InventorySlots.MEGATON_HAMMER
-      );
-    } else {
-      this.removeItem(InventoryItem.MEGATON_HAMMER);
-    }
+    let value = bool ? InventoryItem.MEGATON_HAMMER : InventoryItem.NONE;
+    this.setItemInSlot(value, InventorySlots.MEGATON_HAMMER);
   }
+
   get bottle_1(): InventoryItem {
     return this.getItemInSlot(InventorySlots.BOTTLE1);
   }
   set bottle_1(content: InventoryItem) {
+    if (content < InventoryItem.EMPTY_BOTTLE ||
+      content > InventoryItem.BOTTLED_POE) return;
     this.setItemInSlot(content, InventorySlots.BOTTLE1);
   }
   get bottle_2(): InventoryItem {
     return this.getItemInSlot(InventorySlots.BOTTLE2);
   }
   set bottle_2(content: InventoryItem) {
+    if (content < InventoryItem.EMPTY_BOTTLE ||
+      content > InventoryItem.BOTTLED_POE) return;
     this.setItemInSlot(content, InventorySlots.BOTTLE2);
   }
   get bottle_3(): InventoryItem {
     return this.getItemInSlot(InventorySlots.BOTTLE3);
   }
   set bottle_3(content: InventoryItem) {
+    if (content < InventoryItem.EMPTY_BOTTLE ||
+      content > InventoryItem.BOTTLED_POE) return;
     this.setItemInSlot(content, InventorySlots.BOTTLE3);
   }
   get bottle_4(): InventoryItem {
     return this.getItemInSlot(InventorySlots.BOTTLE4);
   }
   set bottle_4(content: InventoryItem) {
+    if (content < InventoryItem.EMPTY_BOTTLE ||
+      content > InventoryItem.BOTTLED_POE) return;
     this.setItemInSlot(content, InventorySlots.BOTTLE4);
   }
+
   hasBottle(): boolean {
-    for (let i = 0; i <= InventorySlots.CHILD_TRADE_ITEM; i++) {
+    for (let i = InventorySlots.BOTTLE1; i <= InventorySlots.BOTTLE4; i++) {
       let item: InventoryItem = this.getItemInSlot(i);
       if (
         item >= InventoryItem.EMPTY_BOTTLE &&
         item <= InventoryItem.BOTTLED_POE
-      ) {
-        return true;
-      }
+      ) return true;
     }
     return false;
   }
   getBottleCount(): number {
     let bottles = 0;
-    for (let i = 0; i <= InventorySlots.CHILD_TRADE_ITEM; i++) {
+    for (let i = InventorySlots.BOTTLE1; i <= InventorySlots.BOTTLE4; i++) {
       let item: InventoryItem = this.getItemInSlot(i);
       if (
         item >= InventoryItem.EMPTY_BOTTLE &&
         item <= InventoryItem.BOTTLED_POE
-      ) {
-        bottles++;
-      }
+      ) bottles++;
     }
     return bottles;
   }
   getBottledItems(): InventoryItem[] {
     let bottles: InventoryItem[] = new Array();
-    for (let i = 0; i <= InventorySlots.CHILD_TRADE_ITEM; i++) {
+    for (let i = InventorySlots.BOTTLE1; i <= InventorySlots.BOTTLE4; i++) {
       let item: InventoryItem = this.getItemInSlot(i);
       if (
         item >= InventoryItem.EMPTY_BOTTLE &&
         item <= InventoryItem.BOTTLED_POE
-      ) {
-        bottles.push(item);
-      }
+      ) bottles.push(item);
     }
     return bottles;
   }
+
   get childTradeItem(): InventoryItem {
     return this.getItemInSlot(InventorySlots.CHILD_TRADE_ITEM);
   }
   set childTradeItem(item: InventoryItem) {
+    if (item < InventoryItem.WEIRD_EGG ||
+      item > InventoryItem.SOLD_OUT) return;
     this.setItemInSlot(item, InventorySlots.CHILD_TRADE_ITEM);
   }
+
   get adultTradeItem(): InventoryItem {
     return this.getItemInSlot(InventorySlots.ADULT_TRADE_ITEM);
   }
   set adultTradeItem(item: InventoryItem) {
+    if (item < InventoryItem.POCKET_EGG ||
+      item > InventoryItem.CLAIM_CHECK) return;
     this.setItemInSlot(item, InventorySlots.ADULT_TRADE_ITEM);
   }
+
   isChildTradeFinished(): boolean {
     // This is going to require more complex flag checks
     return true;
@@ -764,10 +737,11 @@ export class Inventory extends JSONTemplate implements IInventory {
     // This should be done with flags also
     return true;
   }
+
   getItemInSlot(slotId: number): InventoryItem {
-    if (slotId < 0 || slotId > 23) {
+    if (slotId < 0 || slotId > InventorySlots.CHILD_TRADE_ITEM)
       return InventoryItem.NONE;
-    }
+
     let itemId: number = this.emulator.rdramRead8(this.inventory_addr + slotId);
     return itemId as InventoryItem;
   }
@@ -788,13 +762,14 @@ export class Inventory extends JSONTemplate implements IInventory {
     }
     return slots;
   }
+
   hasItem(item: InventoryItem): boolean {
     return this.getSlotForItem(item) != -1;
   }
+
   getAmmoForItem(item: InventoryItem): number {
-    if (!this.hasAmmo(item)) {
-      return 0;
-    }
+    if (!this.hasAmmo(item)) return 0;
+    
     let ammo = 0;
     let slots: number[] = this.getSlotsForItem(item);
     for (let i = 0; i < slots.length; i++) {
@@ -816,21 +791,17 @@ export class Inventory extends JSONTemplate implements IInventory {
     return false;
   }
   getAmmoForSlot(slotId: number): number {
-    if (slotId < 0 || slotId > 0xf) {
-      return 0;
-    }
+    if (slotId < 0 || slotId > 0xf) return 0;
     return this.emulator.rdramRead8(this.inventory_ammo_addr + slotId);
   }
   setAmmoInSlot(slot: number, amount: number): void {
-    if (slot < 0 || slot >= 0xf) {
-      return;
-    }
+    if (slot < 0 || slot >= 0xf) return;
     this.emulator.rdramWrite8(this.inventory_ammo_addr + slot, amount);
   }
+
   setItemInSlot(item: InventoryItem, slot: number): void {
-    if (slot < 0 || slot > InventorySlots.CHILD_TRADE_ITEM) {
+    if (slot < 0 || slot > InventorySlots.CHILD_TRADE_ITEM)
       return;
-    }
     this.emulator.rdramWrite8(this.inventory_addr + slot, item.valueOf());
   }
   giveItem(item: InventoryItem, desiredSlot: InventorySlots) {
