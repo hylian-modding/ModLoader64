6f40405cce56a3fa0a4d328b644c2da87a8b3fee
Denoflions <denvstheworld@gmail.com>
2019-12-22 10:26:02 -0500

    Dummy core

---
+++ src/modloader/cores/DummyCore.ts
@@ -0,0 +1,22 @@
+import { ICore, IModLoaderAPI } from "modloader64_api/IModLoaderAPI";
+import { IRomHeader } from "modloader64_api/IRomHeader";
+
+export class DummyCore implements ICore{
+
+    header!: string;    
+    ModLoader!: IModLoaderAPI;
+    rom_header?: IRomHeader | undefined;
+
+    preinit(): void {
+    }
+
+    init(): void {
+    }
+
+    postinit(): void {
+    }
+
+    onTick(frame?: number | undefined): void {
+    }
+    
+}
