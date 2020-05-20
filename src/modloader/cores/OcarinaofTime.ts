d1c9075fe9773e0bdfc8461c14ab18569048a540
Denoflions <denvstheworld@gmail.com>
2020-05-20 08:35:16 -0400

    Make n64 logo skip togglable, will be needed to fix gz later.

--- src/modloader/cores/OcarinaofTime.ts
+++ src/modloader/cores/OcarinaofTime.ts
@@ -74,6 +74,8 @@ export class OcarinaofTime implements ICore, IOOTCore {
     }
 
     preinit(): void {
+        this.ModLoader.config.registerConfigCategory("OcarinaofTime");
+        this.ModLoader.config.setData("OcarinaofTime", "skipN64Logo", true);
         this.ModLoader.logger.info(
             'OOT VERSION: ' + ROM_VERSIONS[this.rom_header.revision] + '.'
         );
@@ -87,30 +89,30 @@ export class OcarinaofTime implements ICore, IOOTCore {
             this.applyVersionPatch("Rom downgrade in progress... (1.1 -> 1.0)", "RevA.bps", ROM_VERSIONS.N0);
         }
         switch (this.rom_header.revision) {
-        case ROM_VERSIONS.DEBUG:
-            global.ModLoader['save_context'] = 0x15e660;
-            global.ModLoader['global_context_pointer'] = 0x157da0;
-            global.ModLoader['overlay_table'] = 0x1162A0;
-            global.ModLoader['link_instance'] = 0x2245B0;
-            global.ModLoader['gui_isShown'] = 0x1C4357;
-            offsets.state = 0x067C;
-            offsets.state2 = 0x0680;
-            offsets.paused = 0x166600;
-            offsets.raw_anim = 0x0200;
-            this.payloads.push(__dirname + '/OOT/OcarinaofTime_debug.payload');
-            break;
-        default:
-            global.ModLoader['save_context'] = 0x11a5d0;
-            global.ModLoader['global_context_pointer'] = 0x11f248;
-            global.ModLoader['overlay_table'] = 0x0e8530;
-            global.ModLoader['link_instance'] = 0x1daa30;
-            global.ModLoader['gui_isShown'] = global.ModLoader['save_context'] + 0xbe613;
-            offsets.state = 0x066c;
-            offsets.state2 = 0x0670;
-            offsets.paused = 0x1c6fa0;
-            offsets.raw_anim = 0x01F0;
-            this.payloads.push(__dirname + '/OOT/OcarinaofTime.payload');
-            break;
+            case ROM_VERSIONS.DEBUG:
+                global.ModLoader['save_context'] = 0x15e660;
+                global.ModLoader['global_context_pointer'] = 0x157da0;
+                global.ModLoader['overlay_table'] = 0x1162A0;
+                global.ModLoader['link_instance'] = 0x2245B0;
+                global.ModLoader['gui_isShown'] = 0x1C4357;
+                offsets.state = 0x067C;
+                offsets.state2 = 0x0680;
+                offsets.paused = 0x166600;
+                offsets.raw_anim = 0x0200;
+                this.payloads.push(__dirname + '/OOT/OcarinaofTime_debug.payload');
+                break;
+            default:
+                global.ModLoader['save_context'] = 0x11a5d0;
+                global.ModLoader['global_context_pointer'] = 0x11f248;
+                global.ModLoader['overlay_table'] = 0x0e8530;
+                global.ModLoader['link_instance'] = 0x1daa30;
+                global.ModLoader['gui_isShown'] = global.ModLoader['save_context'] + 0xbe613;
+                offsets.state = 0x066c;
+                offsets.state2 = 0x0670;
+                offsets.paused = 0x1c6fa0;
+                offsets.raw_anim = 0x01F0;
+                this.payloads.push(__dirname + '/OOT/OcarinaofTime.payload');
+                break;
         }
     }
 
@@ -226,8 +228,10 @@ export class OcarinaofTime implements ICore, IOOTCore {
         for (let i = 0; i < this.payloads.length; i++) {
             this.ModLoader.payloadManager.parseFile(this.payloads[i]);
         }
-        this.ModLoader.logger.info("Skipping N64 logo screen...");
-        this.ModLoader.emulator.rdramWritePtr8(global.ModLoader['global_context_pointer'], 0x1E1, 0x1);
+        if (this.ModLoader.config.data["OcarinaofTime"]["skipN64Logo"]) {
+            this.ModLoader.logger.info("Skipping N64 logo screen...");
+            this.ModLoader.emulator.rdramWritePtr8(global.ModLoader['global_context_pointer'], 0x1E1, 0x1);
+        }
     }
 
     mapSelectCode(): void {
