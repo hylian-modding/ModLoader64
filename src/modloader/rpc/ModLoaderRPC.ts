56f37b0183ffc292371a7ff66a87fde1bad63b5d
Denoflions <denvstheworld@gmail.com>
2020-05-22 09:17:16 -0400

    Don't crash if two clients exist with RPC on.

--- src/modloader/rpc/ModLoaderRPC.ts
+++ src/modloader/rpc/ModLoaderRPC.ts
@@ -7,13 +7,18 @@ export class ModLoaderRPC {
     private server!: Server;
 
     setup() {
-        this.server = http.createServer(function (req, res) {
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            let resp = new RPC_Receieve_Event(req);
-            bus.emit(RPCEvents.ON_RECEIVE, resp);
-            res.write(JSON.stringify(resp.response));
-            res.end();
-        }).listen(6500);
+        try {
+            this.server = http.createServer(function (req, res) {
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                let resp = new RPC_Receieve_Event(req);
+                bus.emit(RPCEvents.ON_RECEIVE, resp);
+                res.write(JSON.stringify(resp.response));
+                res.end();
+            }).listen(6500);
+            this.server.on('error', (err)=>{
+            });
+        } catch (err) {
+        }
     }
 
 }
