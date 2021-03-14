d9f9755ad6b71b33d97c15ce01f9bd03ee5de3b2
Denoflions <denvstheworld@gmail.com>
2021-03-14 14:50:58 -0400

    Add zip->pak convert for Smurf

--- bin/paker.js
+++ bin/paker.js
@@ -9,15 +9,28 @@ var PakFormat_1 = require("modloader64_api/PakFormat");
 var path_1 = __importDefault(require("path"));
 var adm_zip_1 = __importDefault(require("adm-zip"));
 var fs_extra_1 = __importDefault(require("fs-extra"));
+var recursive = require('recursive-readdir');
+require('mkdir-recursive');
 commander_1["default"].option('-d --dir <dir>', 'base directory');
 commander_1["default"].option('-i --input <pak>', 'pak to unpak');
 commander_1["default"].option('-o, --output <dir>', 'output dir');
 commander_1["default"].option("-j, --json <file>", "input json");
 commander_1["default"].option("-a, --algo <algo>", "compression algo");
+commander_1["default"].option("-c, --convert <zip>", "convert zip to pak");
 commander_1["default"].parse(process.argv);
+if (commander_1["default"].convert !== undefined) {
+    var zipFile = new adm_zip_1["default"](fs_extra_1["default"].readFileSync(commander_1["default"].convert));
+    zipFile.extractAllTo("./");
+    var pak_1 = new PakFormat_1.Pak("./" + path_1["default"].parse(commander_1["default"].convert).name + '.pak');
+    recursive(path_1["default"].join("./", path_1["default"].parse(commander_1["default"].convert).name), function (err, files) {
+        console.log("Total files: " + files.length);
+        for (var i = 0; i < files.length; i++) {
+            pak_1.save_file(files[i], { enabled: true, algo: "DEFL" });
+        }
+        pak_1.update();
+    });
+}
 if (commander_1["default"].dir !== undefined) {
-    var recursive = require('recursive-readdir');
-    require('mkdir-recursive');
     recursive(commander_1["default"].dir, function (err, files) {
         if (commander_1["default"].algo === "zip") {
             var zipFile = new adm_zip_1["default"]();
--- bin/paker.ts
+++ bin/paker.ts
@@ -1,36 +1,49 @@
 #!/usr/bin/env node
 
 import program from 'commander';
-import {IPakFileCompressionOptions, Pak} from 'modloader64_api/PakFormat';
+import { IPakFileCompressionOptions, Pak } from 'modloader64_api/PakFormat';
 import path from 'path';
 import zip from 'adm-zip';
 import fse from 'fs-extra';
+
+let recursive = require('recursive-readdir');
+require('mkdir-recursive');
 
 program.option('-d --dir <dir>', 'base directory');
 program.option('-i --input <pak>', 'pak to unpak');
 program.option('-o, --output <dir>', 'output dir');
 program.option("-j, --json <file>", "input json");
 program.option("-a, --algo <algo>", "compression algo");
-
+program.option("-c, --convert <zip>", "convert zip to pak");
 program.parse(process.argv);
 
-if (program.dir !== undefined) {
-    let recursive = require('recursive-readdir');
-    require('mkdir-recursive');
+if (program.convert !== undefined) {
+    let zipFile: zip = new zip(fse.readFileSync(program.convert));
+    zipFile.extractAllTo("./");
+    let pak: Pak = new Pak("./" + path.parse(program.convert).name + '.pak');
+    recursive(path.join("./", path.parse(program.convert).name), function (err: any, files: string[]) {
+        console.log("Total files: " + files.length);
+        for (let i = 0; i < files.length; i++) {
+            pak.save_file(files[i], { enabled: true, algo: "DEFL" });
+        }
+        pak.update();
+    });
+}
 
+if (program.dir !== undefined) {
     recursive(program.dir, function (err: any, files: string[]) {
-        if (program.algo === "zip"){
+        if (program.algo === "zip") {
             let zipFile: zip = new zip();
             zipFile.addLocalFolder(path.resolve(program.dir), path.parse(program.dir).name);
             zipFile.writeZip(path.resolve(program.output + "/" + path.parse(program.dir).name + '.zip'));
-        }else{
+        } else {
             let pak: Pak = new Pak(program.output + "/" + path.parse(program.dir).name + '.pak');
             console.log("Total files: " + files.length);
             for (let i = 0; i < files.length; i++) {
-                if (program.algo !== undefined){
+                if (program.algo !== undefined) {
                     console.log(i + " / " + files.length);
-                    pak.save_file(files[i], {enabled: true, algo: program.algo});
-                }else{
+                    pak.save_file(files[i], { enabled: true, algo: program.algo });
+                } else {
                     pak.save_file(files[i]);
                 }
             }
@@ -39,9 +52,9 @@ if (program.dir !== undefined) {
     });
 }
 
-if (program.json){
+if (program.json) {
     let pak: Pak = new Pak(program.output);
-    pak.overwriteFileAtIndex(0, fse.readJSONSync(program.json), {enabled: true, algo: "DEFL"} as IPakFileCompressionOptions);
+    pak.overwriteFileAtIndex(0, fse.readJSONSync(program.json), { enabled: true, algo: "DEFL" } as IPakFileCompressionOptions);
     pak.update();
 }
 
