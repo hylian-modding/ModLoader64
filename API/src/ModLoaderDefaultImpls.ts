0bee5099ba3bcff213a6131d22032d19947e822d
Denoflions <denvstheworld@gmail.com>
2022-03-12 00:12:51 -0500

    Fix a potential exploit with nickname shenanigans

--- API/src/ModLoaderDefaultImpls.ts
+++ API/src/ModLoaderDefaultImpls.ts
@@ -2,36 +2,58 @@ import { RemoteInfo } from 'dgram';
 import { INetworkPlayer, IPacketHeader, SocketType } from './NetworkHandler';
 
 export class NetworkPlayer implements INetworkPlayer {
-  nickname: string;
-  uuid: string;
-  data: any = {};
+    readonly uuid: string;
+    __nickname: () => string;
+    data: any = {};
 
-  constructor(nickname: string, uuid: string) {
-      this.nickname = nickname;
-      this.uuid = uuid;
-  }
+    constructor(nickname: string, uuid: string) {
+        var nick = nickname;
+        this.__nickname = () => {
+            return nick;
+        };
+        this.uuid = uuid;
+    }
+
+    get nickname(): string {
+        return this.__nickname();
+    }
+
+    set nickname(s: string) {
+        let temp = s;
+        if (temp.length > 20) temp = temp.substring(0, 20);
+        if (temp.indexOf("\n") > -1) temp = temp.replace("\n", "");
+        var nick = temp;
+        this.__nickname = Object.freeze(() => {
+            return nick;
+        });
+    }
+
+    toJSON(){
+        return {uuid: this.uuid, nickname: this.nickname, data: this.data};
+    }
+
 }
 
 export class Packet implements IPacketHeader {
-  packet_id: string;
-  lobby: string;
-  channel: string;
-  player!: INetworkPlayer;
-  forward: boolean;
-  socketType: SocketType;
+    packet_id: string;
+    lobby: string;
+    channel: string;
+    player!: INetworkPlayer;
+    forward: boolean;
+    socketType: SocketType;
 
-  constructor(
-      packet_id: string,
-      channel: string,
-      lobby: string,
-      forward = true
-  ) {
-      this.packet_id = packet_id;
-      this.channel = channel;
-      this.forward = forward;
-      this.socketType = SocketType.TCP;
-      this.lobby = lobby;
-  }
+    constructor(
+        packet_id: string,
+        channel: string,
+        lobby: string,
+        forward = true
+    ) {
+        this.packet_id = packet_id;
+        this.channel = channel;
+        this.forward = forward;
+        this.socketType = SocketType.TCP;
+        this.lobby = lobby;
+    }
 }
 
 export class UDPPacket extends Packet {
