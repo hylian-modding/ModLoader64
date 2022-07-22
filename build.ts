683590aae0f11f3f1e1c4ea3fecc2c9b5adab667
den <denvstheworld@gmail.com>
2022-07-22 07:16:34 -0400

    Linux stuff

--- build.ts
+++ build.ts
@@ -1,26 +1,27 @@
 import fs from 'fs-extra';
-import os from 'os';
 import asar from 'asar';
 import child_process from 'child_process';
 import fetch from 'node-fetch';
 import AdmZip from 'adm-zip';
 import path from 'path';
 
-const arch: string = os.arch();
-const platform: string = os.platform();
-
-console.log(`${platform} ${arch}`);
-
-const key: string = `${platform}${arch}`;
-
 async function downloadWindowsDeps() {
-    console.log("Getting client files from github...")
+    console.log("Getting windows client files from github...")
     let pak = await fetch("https://github.com/hylian-modding/ModLoader64-Platform-Deps/raw/master/Windows64/emulator.pak");
-    fs.writeFileSync("./emulator.pak", await pak.buffer());
+    fs.writeFileSync("./windows.pak", await pak.buffer());
+}
+
+async function downloadLinuxDeps() {
+    console.log("Getting linux client files from github...")
+    let pak = await fetch("https://github.com/hylian-modding/ModLoader64-Platform-Deps/raw/master/linux/emulator.pak");
+    fs.writeFileSync("./linux.pak", await pak.buffer());
+}
+
+async function doBuild(pak: string, out: string) {
     if (!fs.existsSync("./client")) {
         fs.mkdirSync("./client");
     }
-    child_process.execSync(`paker -i ./emulator.pak -o ./client`);
+    child_process.execSync(`paker -i ${pak} -o ./client`);
     if (!fs.existsSync("./client/node_modules")) {
         fs.mkdirSync("./client/node_modules");
     }
@@ -28,19 +29,18 @@ async function downloadWindowsDeps() {
     asar.createPackage("./client/node_modules", "./client/node_modules.asar").then(() => {
         asar.createPackage("./build", "./client/modloader64.asar").then(() => {
             fs.removeSync("./client/node_modules");
-            asar.createPackage("./client", "./client.asar").then(() => {
+            asar.createPackage("./client", out).then(() => {
                 let zip = new AdmZip();
-                zip.addLocalFile("./client.asar");
-                zip.writeZip("./client.zip");
-                fs.writeFileSync("./client.md5", child_process.execSync(`md5 -i ${path.resolve("./client.zip")}`).toString().trim());
+                zip.addLocalFile(out);
+                zip.writeZip(`./${path.parse(out).name}.zip`);
+                fs.writeFileSync(`./${path.parse(out).name}.md5`, child_process.execSync(`md5 -i ${path.resolve(`./${path.parse(out).name}.zip`)}`).toString().trim());
             });
         });
     });
+    fs.removeSync("./client");
 }
 
-switch (key) {
-    case "win32x64": {
-        downloadWindowsDeps();
-        break;
-    }
-}
+downloadWindowsDeps();
+downloadLinuxDeps();
+doBuild("./windows.pak", "./windows.asar");
+doBuild("./linux.pak", "./linux.asar");
